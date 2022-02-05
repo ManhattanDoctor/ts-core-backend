e4a8a1f019cc08f305f1f0fd5b0d9a965734d8e9
Renat Gubaev <renat.gubaev@gmail.com>
2022-02-05 11:22:35 +0300

    auto commit

--- src/settings/index.ts
+++ src/settings/index.ts
@@ -4,6 +4,5 @@ export * from './ILoggerSettings';
 export * from './IModeSettings';
 export * from './IPrometheusSettings';
 export * from './IWebSettings';
-
 export * from './EnvSettingsStorage';
 export * from './ApplicationBaseSettings';
